3757180f1a0ecd1c242e2fe8dbdae93f0b309e05
k <291810213@qq.com>
2019-05-28 11:06:03 +0800

    update

--- index.js
+++ index.js
@@ -13,11 +13,9 @@ const Conf = {
   suffix:'/*.+(png|jpg|jpeg|PNG|JPG|JPEG)'
 }
 
-
 const headers = {
   "referer": "https://tinypng.com/",
   "user-agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
-  "X-Forwarded-For": new Array(4).fill(0).map(() => parseInt(Math.random() * 255)).join(".")
 }
 
 
